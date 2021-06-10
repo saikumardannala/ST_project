5fdb20682b66d53da4df19d851edf9b464d37b52
saikumardannala <saikumardannala@gmail.com>
2021-06-10 06:37:09 +0200

    environment build testing

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public Main(){
+
+    }
+    public int power_two(int a){
+        int b= a*a;
+        return b;
+    }
+    public int power_three(int c){
+        int d= c*c*c;
+        return d;
+    }
+}
---
+++ src/test/java/Testmain.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Testmain {
+    private static Main st;
+    @BeforeClass
+    public static void setup(){
+        st= new Main();
+    }
+    @Test
+    public void test_power_two(){
+        assertEquals(40, st.power_two(2));
+    }
+    @Test
+    public void test_power_three(){
+        assertEquals(80, st.power_three(2));
+    }
+}
