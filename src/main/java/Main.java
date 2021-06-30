b660d382850a8b435b70ec3a12139c5f93937685
saikumardannala <saikumardannala@gmail.com>
2021-06-30 23:46:28 +0200

    removed code and tests to perform TDD

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,5 @@ public class Main {
     public Main(){
 
     }
-    public int power_two(int a){
-        int b= a*a;
-        return b;
-    }
-    public int power_three(int c){
-        int d= c*c*c;
-        return d;
-    }
+
 }
--- src/test/java/Testmain.java
+++ src/test/java/Testmain.java
@@ -8,12 +8,5 @@ public class Testmain {
     public static void setup(){
         st= new Main();
     }
-    @Test
-    public void test_power_two(){
-        assertEquals(4, st.power_two(2));
-    }
-    @Test
-    public void test_power_three(){
-        assertEquals(8, st.power_three(2));
-    }
+
 }
